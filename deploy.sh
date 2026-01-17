afcceeada358b72552ff07094f6697e48613b3db
BearThreeStones <2724776621@qq.com>
2026-01-17 16:57:39 +0800

    chore: update package.json for version 2.0.0
    - Changed project description to "分享游戏开发和算法的小网站"
    - Updated version from 1.0.0 to 2.0.0
    - Added "license" and "type" fields
    - Updated scripts to use vuepress-vite
    - Updated devDependencies to include new versions and packages

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,16 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 拷贝目录和文件
-cp -r ../../../.github ./
+# cp -r ../../../.github ./
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 推送到 gh-pages 分支
-git push -f https://github.com/BearThreeStones/blog.git master:gh-pages
+# git push -f https://github.com/BearThreeStones/blog.git master:gh-pages
+
+# 推送到远端服务器
+git push -f git@43.138.57.136:/home/www/website/blog.git master
 
 cd -
